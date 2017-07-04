199d8c35cceae56d379281e10e33d93c16fd19b2
Randy Pray <randypray@users.noreply.github.com>
2017-07-03 20:03:44 -0500

    added DisplayMesssageActivity

---
+++ app/src/main/java/com/example/prayr1/myfirstapp/DisplayMessageActivity.java
@@ -0,0 +1,13 @@
+package com.example.prayr1.myfirstapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+    }
+}
